217f0dd7f14e3f498a4280f36d7c00d0571f19c0
Roland Geider <roland@geider.net>
2021-12-20 19:47:45 +0100

    Add test for get_weight service

--- src/services/weight.ts
+++ src/services/weight.ts
@@ -4,11 +4,11 @@ import {ResponseType} from "./responseType";
 
 const BASEURL = 'https://wger-master.rge.uber.space/api/v2/weightentry/'
 
-export const get_weights = async () => {    
+export const get_weights = async () => {
     const {data: received_weights} = await axios.get<ResponseType<BodyWeightType>>(BASEURL, {
-                headers: {
-                    Authorization: process.env.REACT_APP_API_KEY as string
-                }
-            })
-    return received_weights.results    
+        headers: {
+            Authorization: process.env.REACT_APP_API_KEY as string
+        }
+    })
+    return received_weights.results
 };
