ed743f8c31af50bbadb0f9f4169ca93af7078674
Roland Geider <roland@geider.net>
2021-12-20 15:51:04 +0100

    Use generics to make the API response type reusable

--- src/services/weight.ts
+++ src/services/weight.ts
@@ -1,20 +1,14 @@
 import axios from 'axios';
-import { BodyWeightType } from '../types';
+import {BodyWeightType} from '../types';
+import {ResponseType} from "./responseType";
 
 const BASEURL = 'https://wger-master.rge.uber.space/api/v2/weightentry/'
 
-interface ResponseType {
-    count: number,
-    next: number | null,
-    previous: number | null,
-    results: [BodyWeightType]
-}
-
 export const get_weights = async () => {
-    const {data: received_weights} = await axios.get<ResponseType>(BASEURL, {
-                headers: {
-                    Authorization: 'Token 31e2ea0322c07b9df583a9b6d1e794f7139e78d4'
-                }
-            })
+    const {data: received_weights} = await axios.get<ResponseType<BodyWeightType>>(BASEURL, {
+        headers: {
+            Authorization: 'Token 31e2ea0322c07b9df583a9b6d1e794f7139e78d4'
+        }
+    })
     return received_weights.results
 };
