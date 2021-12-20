5f0fb48d20929e9244f641af8cdca62a674e02cf
Saka Ricky <sakaricky91@gmail.com>
2021-12-20 09:29:30 +0100

    feat: added theming and installed packages

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { theme } from './theme';
+import { ThemeProvider } from '@mui/material/styles'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
