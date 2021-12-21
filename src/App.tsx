d3aeb32725cd94c69ac391dd9dd2345a221fb146
Roland Geider <roland@geider.net>
2021-12-21 19:12:33 +0100

    Remove test translation

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,28 @@
 import React from 'react';
 import './App.css';
 import {BodyWeight} from './components';
-import {Trans, useTranslation} from "react-i18next";
+import {useTranslation} from "react-i18next";
 
 
 function App() {
 
-  const {t} = useTranslation();
+    const {t} = useTranslation();
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          <Trans i18nKey="test"/>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <BodyWeight/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+            <BodyWeight/>
+        </div>
+    );
 }
 
 export default App;
