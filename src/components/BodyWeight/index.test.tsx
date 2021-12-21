a3f1b868a1cec15009ecaf75d70c5909702e1bfc
Roland Geider <roland@geider.net>
2021-12-21 20:57:15 +0100

    Test that both weights are found in the rendered component

--- src/components/BodyWeight/index.test.tsx
+++ src/components/BodyWeight/index.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {render, screen, waitFor} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import {get_weights} from "services";
 import {BodyWeight} from "./index";
-import { StateProvider } from 'state'
+import {StateProvider} from 'state'
 
 jest.mock("axios");
 jest.mock("services");
@@ -11,21 +11,21 @@ jest.mock("services");
 test('renders without crashing', async () => {
 
     const weightData = [
-        {id: 1, user:3, weight: '80', date: '2021-12-10'},
-        {id: 2, user:2, weight: '90', date: '2021-12-20'},
+        {id: 1, user: 3, weight: '80', date: '2021-12-10'},
+        {id: 2, user: 2, weight: '90', date: '2021-12-20'},
     ];
 
     // @ts-ignore
     get_weights.mockImplementation(() => Promise.resolve(weightData));
+
     // since I used context api to provide state, also need it here
     render(<StateProvider><BodyWeight/></StateProvider>);
     expect(get_weights).toHaveBeenCalledTimes(1);
 
     // Both weights are found in th document
-    // TODO: this still fails. Why?? Using waitFor for components that
-    // update state asynchronously
-    await waitFor(() => {
-        const linkElement = screen.getByText("80");
-        expect(linkElement).toBeInTheDocument();
-    })
+    const linkElement = await screen.findByText("80");
+    expect(linkElement).toBeInTheDocument();
+
+    const linkElement2 = await screen.findByText("90");
+    expect(linkElement2).toBeInTheDocument();
 });
