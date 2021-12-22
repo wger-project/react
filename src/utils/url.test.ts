29894a01a3944b958a11f750c70dc3f27434c83f
Roland Geider <roland@geider.net>
2021-12-22 13:55:50 +0100

    Tests are not async

--- src/utils/url.test.ts
+++ src/utils/url.test.ts
@@ -2,12 +2,12 @@ import { make_url } from "utils/url";
 
 describe("test url utility", () => {
 
-    test('generate overview URL', async () => {
+    test('generate overview URL', () => {
         const result = make_url('https://example.com', 'endpoint');
         expect(result).toStrictEqual('https://example.com/api/v2/endpoint/');
     });
 
-    test('generate detail URL', async () => {
+    test('generate detail URL', () => {
         const result = make_url('https://example.com', 'endpoint', 1);
         expect(result).toStrictEqual('https://example.com/api/v2/endpoint/1/');
     });
