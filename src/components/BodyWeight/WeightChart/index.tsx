5eed53b7cff5d38e32831693d022701f1f2bd11b
Roland Geider <roland@geider.net>
2021-12-28 20:28:57 +0100

    Handle clicks on currently selected weights

--- src/components/BodyWeight/WeightChart/index.tsx
+++ src/components/BodyWeight/WeightChart/index.tsx
@@ -1,4 +1,4 @@
-import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { CartesianGrid, DotProps, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import React from 'react';
 import { WeightTableProps } from "components/BodyWeight/Table";
 import i18n from "i18next";
@@ -11,9 +11,18 @@ export const WeightChart = ({ weights }: WeightTableProps) => {
             // Format date according to the locale
             date: new Date(weight.date).toLocaleDateString(i18n.language),
             //date: weight.date.toISOString().substring(0, 10),
-            weight: weight.weight
+            weight: weight.weight,
+            id: weight.id
         };
     });
+
+    /*
+     * Edit the currently selected weight
+     */
+    function handleClick(e: DotProps, bar: any) {
+        //TODO: open a modal with the weight details to allow the user to edit it
+        console.log(bar.payload);
+    }
 
     return (
         <ResponsiveContainer width="90%" height={400}>
@@ -23,8 +32,13 @@ export const WeightChart = ({ weights }: WeightTableProps) => {
                     dataKey="weight"
                     stroke="#2A4C7D"
                     strokeWidth={4}
-                    dot={{ strokeWidth: 4 }}
-                    activeDot={{ stroke: '#2A4C7D', strokeWidth: 2, r: 6 }}/>
+                    dot={{ strokeWidth: 2, r: 7 }}
+                    activeDot={{
+                        stroke: '#2A4C7D',
+                        strokeWidth: 2,
+                        r: 6,
+                        onClick: handleClick
+                    }}/>
                 <CartesianGrid
                     stroke="#ccc"
                     strokeDasharray="5 5"/>
