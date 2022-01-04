84b6c954ee813785084c5ce7406a2b1623f16444
Roland Geider <roland@geider.net>
2022-01-04 15:12:03 +0100

    Start adding forms to edit/create weight entries

---
+++ src/components/BodyWeight/Form/index.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { WeightEntry } from "components/BodyWeight/model";
+import * as yup from 'yup';
+import { useFormik } from "formik";
+import { Button, TextField } from "@mui/material";
+
+
+//{ weight }: WeightEntry
+export const WeightForm = () => {
+
+    const weight = new WeightEntry(new Date('2020-01-01'), 100, 1);
+
+    const validationSchema = yup.object({
+        weight: yup
+            .number()
+            .min(30, 'Min weight is 30 kg')
+            .max(300, 'Max weight is 300 kg')
+            .required('Weight field is required'),
+    });
+
+    const formik = useFormik({
+        initialValues: {
+            weight: weight.weight,
+            date: weight.date.toISOString().split('T')[0],
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+            console.log(JSON.stringify(values, null, 2));
+        },
+    });
+
+    return (
+        <div>
+            <form onSubmit={formik.handleSubmit}>
+
+                <TextField
+                    fullWidth
+                    id="weight"
+                    name="weight"
+                    label="Weight"
+                    value={formik.values.weight}
+                    onChange={formik.handleChange}
+                    error={formik.touched.weight && Boolean(formik.errors.weight)}
+                    helperText={formik.touched.weight && formik.errors.weight}
+                />
+                <TextField
+                    fullWidth
+                    id="date"
+                    name="date"
+                    label="Date"
+                    type="date"
+                    value={formik.values.date}
+                    onChange={formik.handleChange}
+                    error={formik.touched.date && Boolean(formik.errors.date)}
+                    helperText={formik.touched.date && formik.errors.date}
+                />
+                <Button color="primary" variant="contained" type="submit">
+                    Submit
+                </Button>
+            </form>
+        </div>
+    );
+};
--- src/components/BodyWeight/Table/ActionButton/index.tsx
+++ src/components/BodyWeight/Table/ActionButton/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { ProcessedWeight } from '..';
-import { Button, Menu, MenuItem } from "@mui/material";
+import { Button, Card, CardActions, CardContent, CardHeader, Menu, MenuItem, Modal } from "@mui/material";
 import { Trans } from "react-i18next";
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { WeightForm } from "components/BodyWeight/Form";
 
 interface ActionButtonProps {
     weight: ProcessedWeight
@@ -10,6 +11,7 @@ interface ActionButtonProps {
 
 export const ActionButton = ({ weight }: ActionButtonProps) => {
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const [openModal, setOpenModal] = React.useState(false);
     const open = Boolean(anchorEl);
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         setAnchorEl(event.currentTarget);
@@ -17,6 +19,30 @@ export const ActionButton = ({ weight }: ActionButtonProps) => {
     const handleClose = () => {
         setAnchorEl(null);
     };
+
+    const handleEdit = () => {
+        console.log("Edit clicked");
+        handleClose();
+        handleOpenModal();
+    };
+
+    const handleDelete = () => {
+        console.log("Delete clicked");
+        handleClose();
+    };
+
+    const handleOpenModal = () => setOpenModal(true);
+    const handleCloseModal = () => setOpenModal(false);
+
+    const style = {
+        position: 'absolute' as 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        p: 2,
+        minWidth: '400px'
+    };
+
 
     return (
         <div>
@@ -31,9 +57,26 @@ export const ActionButton = ({ weight }: ActionButtonProps) => {
                     'aria-labelledby': 'basic-button',
                 }}
             >
-                <MenuItem onClick={handleClose}><Trans i18nKey={"edit"}/></MenuItem>
-                <MenuItem onClick={handleClose}><Trans i18nKey={"delete"}/></MenuItem>
+                <MenuItem onClick={handleEdit}><Trans i18nKey={"edit"}/></MenuItem>
+                <MenuItem onClick={handleDelete}><Trans i18nKey={"delete"}/></MenuItem>
             </Menu>
+            <Modal
+                open={openModal}
+                onClose={handleCloseModal}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Card sx={style}>
+                    <CardHeader title="Title" subheader="September 14, 2016"/>
+                    <CardContent>
+                        <WeightForm/>
+                        <p>Content goes here...</p>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small" onClick={handleCloseModal}>Close</Button>
+                    </CardActions>
+                </Card>
+            </Modal>
         </div>
     );
 };
