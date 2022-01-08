90485aefed508845b9aa3efef3b8804188c29912
Roland Geider <roland@geider.net>
2022-01-08 12:34:22 +0100

    Open modal allowing user to edit the clicked entry on the diagram

--- src/components/BodyWeight/WeightChart/index.tsx
+++ src/components/BodyWeight/WeightChart/index.tsx
@@ -1,53 +1,68 @@
 import { CartesianGrid, DotProps, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import React from 'react';
 import { WeightTableProps } from "components/BodyWeight/Table";
-import i18n from "i18next";
+import i18n, { t } from "i18next";
+import { WeightEntry } from "components/BodyWeight/model";
+import { WeightForm } from "components/BodyWeight/Form/WeightForm";
+import { WgerModal } from "components/Core/WgerModal";
 
 // don't require the "fetchNewWeights" prop from the WeightTableProps
 export const WeightChart = ({ weights }: Omit<WeightTableProps, "fetchNewWeights">) => {
+
+    const [isModalOpen, setIsModalOpen] = React.useState(false);
+    const [currentEntry, setCurrentEntry] = React.useState<WeightEntry>();
+    const handleCloseModal = () => setIsModalOpen(false);
 
     // map the list of weights to an array of objects with the date and weight
     const weightData = weights.map(weight => {
         return {
             // Format date according to the locale
             date: new Date(weight.date).toLocaleDateString(i18n.language),
-            //date: weight.date.toISOString().substring(0, 10),
             weight: weight.weight,
-            id: weight.id
+            entry: weight
         };
     });
 
     /*
      * Edit the currently selected weight
      */
-    function handleClick(e: DotProps, bar: any) {
-        //TODO: open a modal with the weight details to allow the user to edit it
-        console.log(bar.payload);
+    function handleClick(e: DotProps, data: any) {
+        setCurrentEntry(data.payload.entry);
+        setIsModalOpen(true);
     }
 
-    return (
-        <ResponsiveContainer width="90%" height={400}>
-            <LineChart data={weightData}>
-                <Line
-                    type="monotone"
-                    dataKey="weight"
-                    stroke="#2A4C7D"
-                    strokeWidth={4}
-                    dot={{ strokeWidth: 2, r: 7 }}
-                    activeDot={{
-                        stroke: '#2A4C7D',
-                        strokeWidth: 2,
-                        r: 6,
-                        onClick: handleClick
-                    }}/>
-                <CartesianGrid
-                    stroke="#ccc"
-                    strokeDasharray="5 5"/>
-                <XAxis dataKey="date"/>
-                <YAxis domain={['auto', 'auto']}/>
 
-                <Tooltip/>
-            </LineChart>
-        </ResponsiveContainer>
+    return (
+        <div>
+            {
+                currentEntry &&
+                <WgerModal title={t('edit')} isOpen={isModalOpen} closeFn={handleCloseModal}>
+                    <WeightForm weightEntry={currentEntry} />
+                </WgerModal>
+            }
+            <ResponsiveContainer width="90%" height={400}>
+                <LineChart data={weightData}>
+                    <Line
+                        type="monotone"
+                        dataKey="weight"
+                        stroke="#2A4C7D"
+                        strokeWidth={4}
+                        dot={{ strokeWidth: 2, r: 7 }}
+                        activeDot={{
+                            stroke: '#2A4C7D',
+                            strokeWidth: 2,
+                            r: 6,
+                            onClick: handleClick
+                        }} />
+                    <CartesianGrid
+                        stroke="#ccc"
+                        strokeDasharray="5 5" />
+                    <XAxis dataKey="date" />
+                    <YAxis domain={['auto', 'auto']} />
+
+                    <Tooltip />
+                </LineChart>
+            </ResponsiveContainer>
+        </div>
     );
 };
