cb17bc6a5c54258785fa9ff7319071580147df16
Roland Geider <roland@geider.net>
2022-01-10 17:02:20 +0100

    Add simple test that checks that the correct function gets called

--- src/components/BodyWeight/Form/WeightForm.test.tsx
+++ src/components/BodyWeight/Form/WeightForm.test.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { WeightEntry } from "components/BodyWeight/model";
 import { WeightForm } from "components/BodyWeight/Form/WeightForm";
+import * as weightService from "services/weight";
+
+jest.mock("services/weight");
 
 describe("Test WeightForm component", () => {
     test('Passing an existing entry renders its values in the form', () => {
@@ -22,6 +25,27 @@ describe("Test WeightForm component", () => {
         expect(screen.getByLabelText('date')).toBeInTheDocument();
         expect(screen.getByLabelText('weight')).toBeInTheDocument();
         expect(screen.getByText('submit')).toBeInTheDocument();
+    });
+
+    test('Editing an existing entry', async () => {
+
+        // Arrange
+        const weightEntry: WeightEntry = {
+            id: 1,
+            date: new Date('2021-12-10'),
+            weight: 80
+        };
+
+        // Act
+        await render(<WeightForm weightEntry={weightEntry} />);
+        const button = screen.getByRole('button', { name: 'submit' });
+
+        // Assert
+        expect(button).toBeInTheDocument();
+        fireEvent.click(button);
+        await waitFor(() => {
+            expect(weightService.updateWeight).toHaveBeenCalledTimes(1);
+        });
     });
 
 });
