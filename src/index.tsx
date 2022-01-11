e23405733e40af94e4c0a7e1d93893d89a2790af
Roland Geider <roland@geider.net>
2022-01-11 18:21:44 +0100

    Needed changes to integrate react components into the django application

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, {Suspense} from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './i18n';
@@ -6,24 +6,37 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {theme} from './theme';
-import {ThemeProvider} from '@mui/material/styles';
+import { theme } from 'theme';
+import { ThemeProvider } from '@mui/material/styles';
 import { StateProvider } from 'state';
+import { WeightOverview } from "pages";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Suspense fallback={<div>Loading... </div>}>
-      <Router>
-        <StateProvider>
-          <ThemeProvider theme={theme}>
-            <App/>
-          </ThemeProvider>
-        </StateProvider>
-      </Router>
-    </Suspense>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById("root");
+if (rootElement) {
+    ReactDOM.render(
+        <React.StrictMode>
+            <Suspense fallback={<div>Loading... </div>}>
+                <Router>
+                    <StateProvider>
+                        <ThemeProvider theme={theme}>
+                            <App />
+                        </ThemeProvider>
+                    </StateProvider>
+                </Router>
+            </Suspense>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+
+/*
+ * Components used in the wger django app, don't change the IDs here
+ */
+const weightOverview = document.getElementById("weight-overview");
+if (weightOverview) {
+    ReactDOM.render(<WeightOverview />, weightOverview);
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
