09220e7e74a419f96fc34622a2134afe4afee33f
Roland Geider <roland@geider.net>
2022-01-27 10:21:50 +0100

    Set colors palette used in the web and mobile app

--- src/theme.ts
+++ src/theme.ts
@@ -6,6 +6,23 @@ const themeOptions: ThemeOptions = {
     typography: {
         fontFamily: `'Open Sans Light'`
     },
+    palette: {
+        primary: {
+            main: '#2A4C7D',
+        },
+        secondary: {
+            main: '#e63946',
+        },
+        warning: {
+            main: '#cba328',
+        },
+        info: {
+            main: '#457b9d',
+        },
+        success: {
+            main: '#307916',
+        },
+    }
 };
 
 export const theme = createTheme(themeOptions);
