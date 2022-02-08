8ea1306a75b983a381bcf1902f534a3aa5d3c68e
Roland Geider <roland@geider.net>
2022-02-08 20:02:39 +0100

    Revert previous commit, this was causing a bug

--- src/components/BodyWeight/utils.ts
+++ src/components/BodyWeight/utils.ts
@@ -4,21 +4,19 @@ export const processWeight = (weights: WeightEntry[]) => {
     // go through weights, referencing the same weights to have days and weight changes
     const processedWeights = weights.map((entry, i) => {
 
-        const dayDiff = Math.abs(entry.date.getTime() - entry.date.getTime()) / (1000 * 60 * 60 * 24);
-
         // since there is no day before day 1, changes are 0
         if (i === 0) {
             return {
                 entry,
                 change: 0,
-                days: dayDiff
+                days: Math.abs(entry.date.getTime() - entry.date.getTime()) / (1000 * 60 * 60 * 24)
             };
         }
 
         return {
             entry,
             change: weights[i].weight - weights[i - 1].weight,
-            days: dayDiff
+            days: Math.abs(entry.date.getTime() - weights[i - 1].date.getTime()) / (1000 * 60 * 60 * 24)
         };
     });
 
