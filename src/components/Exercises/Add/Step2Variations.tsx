6bec3b910f5bfd5dafe33773343cacbae2dff601
Roland Geider <roland@geider.net>
2022-06-22 17:31:26 +0200

    Start working on variations widget

--- src/components/Exercises/Add/Step2Variations.tsx
+++ src/components/Exercises/Add/Step2Variations.tsx
@@ -1,13 +1,64 @@
 import React from "react";
-import { Box, Button, Typography } from "@mui/material";
+import {
+    Avatar,
+    Box,
+    Button,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemButton,
+    ListItemText,
+    Paper,
+    Switch,
+    Typography
+} from "@mui/material";
 import { useTranslation } from "react-i18next";
+import { useBasesQuery } from "components/Exercises/queries";
+import { ExerciseBase } from "components/Exercises/models/exerciseBase";
+import { LoadingWidget } from "components/Core/LoadingWidget/LoadingWidget";
+
+//const ExerciseInfoListItem = () =>
+
+
+// New component that displays the exercise info in a ListItem
+const ExerciseInfoListItem = ({ exerciseBase }: { exerciseBase: ExerciseBase }) => {
+    return <>
+        <ListItem disablePadding secondaryAction={
+            <Switch />
+        }>
+            <ListItemButton>
+                <ListItemAvatar>
+                    <Avatar src="https://mui.com/static/images/cards/contemplative-reptile.jpg">
+                    </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={exerciseBase.getTranslation().name} secondary="Jan 9, 2014" />
+            </ListItemButton>
+        </ListItem>
+    </>;
+};
+
 
 export const Step2Variations = (props: { onContinue: React.MouseEventHandler<HTMLButtonElement> | undefined; onBack: React.MouseEventHandler<HTMLButtonElement> | undefined; }) => {
     const [t] = useTranslation();
+    const basesQuery = useBasesQuery();
 
     return <>
-        <Typography>{t('forms.whatVariationsExist')}</Typography>
-        --Variations widget goes here--
+        <Paper sx={{ p: 2 }}>
+            <Typography>{t('forms.whatVariationsExist')}</Typography>
+
+            {basesQuery.isLoading ? (
+                <Box>
+                    <LoadingWidget />
+                </Box>
+            ) : (
+                <List style={{ maxHeight: "400px", overflowY: "scroll" }}>
+                    {basesQuery.data!.filter(base => base.variationId === null).map(base => (
+                        <ExerciseInfoListItem exerciseBase={base} key={base.id} />
+                    ))}
+                </List>
+            )}
+        </Paper>
+
         <Box sx={{ mb: 2 }}>
             <div>
                 <Button
