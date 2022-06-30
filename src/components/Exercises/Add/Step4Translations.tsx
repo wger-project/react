3048899cba305176e58dc6c5f8cb32e287ddf925
Roland Geider <roland@geider.net>
2022-06-30 15:50:01 +0200

    Only show the rest of the form when actually translating the exercise

--- src/components/Exercises/Add/Step4Translations.tsx
+++ src/components/Exercises/Add/Step4Translations.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Button, FormControlLabel, FormGroup, Stack, Switch, TextField } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { Form, Formik } from "formik";
 import * as yup from "yup";
+import { addExerciseDataType } from "components/Exercises/models/exerciseBase";
 
-export const Step4Translations = (props: { onContinue: React.MouseEventHandler<HTMLButtonElement> | undefined; onBack: React.MouseEventHandler<HTMLButtonElement> | undefined; }) => {
+
+type Step4BasicsProps = {
+    onContinue: () => void;
+    onBack: () => void;
+    setNewExerciseData: React.Dispatch<React.SetStateAction<addExerciseDataType>>;
+    newExerciseData: addExerciseDataType;
+};
+
+export const Step4Translations = ({
+                                      setNewExerciseData,
+                                      newExerciseData,
+                                      onContinue,
+                                      onBack,
+                                  }: Step4BasicsProps) => {
     const [t] = useTranslation();
+    const [translateExercise, setTranslateExercise] = useState<boolean>(false);
 
     const validationSchema = yup.object({
         description: yup
@@ -30,75 +45,79 @@ export const Step4Translations = (props: { onContinue: React.MouseEventHandler<H
         }}
         validationSchema={validationSchema}
         onSubmit={(values) => {
-
             console.log('Submitting the form with values: ', values);
-
-            // @ts-ignore
-            props.onContinue!(undefined);
+            onContinue();
         }}
     >{formik => (
         <Form>
             <Stack spacing={2}>
                 <FormGroup>
-                    <FormControlLabel control={<Switch />} label="Translate this exercise now" />
+                    <FormControlLabel checked={translateExercise}
+                                      onClick={() => setTranslateExercise(!translateExercise)}
+                                      control={<Switch />}
+                                      label="Translate this exercise now" />
                 </FormGroup>
-                <TextField
-                    id="name"
-                    label="Name"
-                    variant="standard"
-                    error={
-                        Boolean(formik.errors.name && formik.touched.name)
-                    }
-                    helperText={
-                        Boolean(formik.errors.name && formik.touched.name)
-                            ? formik.errors.name
-                            : ''
-                    }
-                    {...formik.getFieldProps('name')}
-                />
-                <TextField
-                    id="alternative-names"
-                    label="Alternative names"
-                    rows={3}
-                    variant="standard"
-                    error={
-                        Boolean(formik.errors.alternativeNames && formik.touched.alternativeNames)
-                    }
-                    helperText={
-                        Boolean(formik.errors.alternativeNames && formik.touched.alternativeNames)
-                            ? formik.errors.alternativeNames
-                            : ''
-                    }
-                    {...formik.getFieldProps('alternativeNames')}
-                />
-                <TextField
-                    id="description"
-                    label={t('description')}
-                    variant="standard"
-                    rows={3}
-                    error={
-                        Boolean(formik.errors.description && formik.touched.description)
-                    }
-                    helperText={
-                        Boolean(formik.errors.description && formik.touched.description)
-                            ? formik.errors.description
-                            : ''
-                    }
-                    {...formik.getFieldProps('description')}
-                />
+                {translateExercise && (
+                    <>
+                        <TextField
+                            id="name"
+                            label="Name"
+                            variant="standard"
+                            error={
+                                Boolean(formik.errors.name && formik.touched.name)
+                            }
+                            helperText={
+                                Boolean(formik.errors.name && formik.touched.name)
+                                    ? formik.errors.name
+                                    : ''
+                            }
+                            {...formik.getFieldProps('name')}
+                        />
+                        <TextField
+                            id="alternative-names"
+                            label="Alternative names"
+                            rows={3}
+                            variant="standard"
+                            error={
+                                Boolean(formik.errors.alternativeNames && formik.touched.alternativeNames)
+                            }
+                            helperText={
+                                Boolean(formik.errors.alternativeNames && formik.touched.alternativeNames)
+                                    ? formik.errors.alternativeNames
+                                    : ''
+                            }
+                            {...formik.getFieldProps('alternativeNames')}
+                        />
+                        <TextField
+                            id="description"
+                            label={t('description')}
+                            variant="standard"
+                            rows={3}
+                            error={
+                                Boolean(formik.errors.description && formik.touched.description)
+                            }
+                            helperText={
+                                Boolean(formik.errors.description && formik.touched.description)
+                                    ? formik.errors.description
+                                    : ''
+                            }
+                            {...formik.getFieldProps('description')}
+                        />
+                    </>
+                )}
             </Stack>
             <Box sx={{ mb: 2 }}>
                 <div>
                     <Button
                         variant="contained"
-                        onClick={props.onContinue}
+                        onClick={onContinue}
                         sx={{ mt: 1, mr: 1 }}
                     >
                         {t('continue')}
                     </Button>
                     <Button
                         disabled={false}
-                        onClick={props.onBack}
+                        onClick={onBack}
                         sx={{ mt: 1, mr: 1 }}
                     >
                         {t('back')}
