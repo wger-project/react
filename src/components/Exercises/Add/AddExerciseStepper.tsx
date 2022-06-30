1104d98f0966b7c6d09519f8fb2a5e54ea332b4b
Roland Geider <roland@geider.net>
2022-06-30 15:50:27 +0200

    Pass all props

--- src/components/Exercises/Add/AddExerciseStepper.tsx
+++ src/components/Exercises/Add/AddExerciseStepper.tsx
@@ -20,135 +20,138 @@ import { Step5Images } from "components/Exercises/Add/Step5Images";
 import { addExerciseDataType } from "components/Exercises/models/exerciseBase";
 
 export const AddExerciseStepper = () => {
-	const [t] = useTranslation();
-	const [activeStep, setActiveStep] = React.useState(0);
+    const [t] = useTranslation();
+    const [activeStep, setActiveStep] = React.useState(0);
 
-	const handleNext = () => {
-		setActiveStep(prevActiveStep => prevActiveStep + 1);
-	};
+    const handleNext = () => {
+        setActiveStep(prevActiveStep => prevActiveStep + 1);
+    };
 
-	const handleBack = () => {
-		setActiveStep(prevActiveStep => prevActiveStep - 1);
-	};
+    const handleBack = () => {
+        setActiveStep(prevActiveStep => prevActiveStep - 1);
+    };
 
-	const handleReset = () => {
-		setActiveStep(0);
-	};
+    const handleReset = () => {
+        setActiveStep(0);
+    };
 
-	const emptyExerciseData = {
-		nameEn: "",
-		descriptionEn: "",
-		alternativeNamesEn: [],
-		category: "",
-		muscles: [],
-		musclesSecondary: [],
-		variationId: null,
-		languageId: null,
-		nameTranslation: "",
-		alternativeNamesTranslation: [],
-		descriptionTranslation: "",
-		images: [],
-	};
-	const [newExerciseData, setNewExerciseData] =
-		React.useState<addExerciseDataType>(emptyExerciseData);
+    const emptyExerciseData = {
+        nameEn: "",
+        descriptionEn: "",
+        alternativeNamesEn: [],
+        category: "",
+        muscles: [],
+        musclesSecondary: [],
+        variationId: null,
+        languageId: null,
+        nameTranslation: "",
+        alternativeNamesTranslation: [],
+        descriptionTranslation: "",
+        images: [],
+    };
+    const [newExerciseData, setNewExerciseData] =
+        React.useState<addExerciseDataType>(emptyExerciseData);
 
-	return (
-		<Container maxWidth="md">
-			<Stack direction={"row"}>
-				<Typography gutterBottom variant="h3" component="div">
-					{t("exercises.contribute-exercise")}
-				</Typography>
-			</Stack>
+    return (
+        <Container maxWidth="md">
+            <Stack direction={"row"}>
+                <Typography gutterBottom variant="h3" component="div">
+                    {t("exercises.contribute-exercise")}
+                </Typography>
+            </Stack>
 
-			<Stack>
-				<Paper sx={{ p: 1, bgcolor: "lightgray" }}>
-					<Typography gutterBottom variant="h5" component="div">
-						Debug submitted data
-					</Typography>
-					<ul>
-						<li>Name EN: {newExerciseData!.nameEn}</li>
-						<li>Description EN: {newExerciseData!.descriptionEn}</li>
-						<li>
-							Alternative names EN:{" "}
-							{newExerciseData!.alternativeNamesEn.join(", ")}
-						</li>
-						<li>Description EN: {newExerciseData!.descriptionEn}</li>
-						<li>Category ID: {newExerciseData!.category}</li>
-						<li>Muscles: {newExerciseData!.muscles}</li>
-						<li>Muscles secondary: {newExerciseData!.musclesSecondary}</li>
-						<li>Variation ID: {newExerciseData!.variationId}</li>
-						<li>Language ID: {newExerciseData!.languageId}</li>
-						<li>Name translation: {newExerciseData!.nameTranslation}</li>
-						<li>
-							Description translation: {newExerciseData!.descriptionTranslation}
-						</li>
-						<li>
-							Alternative names translation:{" "}
-							{newExerciseData!.alternativeNamesTranslation}
-						</li>
-						<li>Images: {newExerciseData!.images}</li>
-					</ul>
-				</Paper>
-			</Stack>
-			<Box>
-				<Typography gutterBottom variant="h5" component="div">
-					{t("exercises.contribute-exercise-description")}
-				</Typography>
-				<Stepper activeStep={activeStep} orientation="vertical">
-					<Step key={1}>
-						<StepLabel>Basics in English</StepLabel>
-						<StepContent>
-							<Step1Basics
-								onContinue={handleNext}
-								setNewExerciseData={setNewExerciseData}
-								newExerciseData={newExerciseData}
-							/>
-						</StepContent>
-					</Step>
-					<Step key={2}>
-						<StepLabel>Variations</StepLabel>
-						<StepContent>
-							<Step2Variations onContinue={handleNext} onBack={handleBack} />
-						</StepContent>
-					</Step>
-					<Step key={3}>
-						<StepLabel>Description</StepLabel>
-						<StepContent>
-							<Step3Description
-								onContinue={handleNext}
-								onBack={handleBack}
-								setNewExerciseData={setNewExerciseData}
-								newExerciseData={newExerciseData}
-							/>
-						</StepContent>
-					</Step>
-					<Step key={4}>
-						<StepLabel>Translation</StepLabel>
-						<StepContent>
-							<Step4Translations onContinue={handleNext} onBack={handleBack} />
-						</StepContent>
-					</Step>
-					<Step key={5}>
-						<StepLabel>Images</StepLabel>
-						<StepContent>
-							<Step5Images
-								onContinue={handleNext}
-								onBack={handleBack}
-								setNewExerciseData={setNewExerciseData}
-								newExerciseData={newExerciseData}
-							/>
-						</StepContent>
-					</Step>
-				</Stepper>
-				{activeStep === 5 && (
-					<Paper square sx={{ p: 3 }}>
-						<Typography>Exercise was successfully submitted!</Typography>
-						<Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
-							Reset
-						</Button>
-					</Paper>
-				)}
-			</Box>
-		</Container>
-	);
+            <Stack>
+                <Paper sx={{ p: 1, bgcolor: "lightgray" }}>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Debug submitted data
+                    </Typography>
+                    <ul>
+                        <li>Name EN: {newExerciseData!.nameEn}</li>
+                        <li>Description EN: {newExerciseData!.descriptionEn}</li>
+                        <li>
+                            Alternative names EN:{" "}
+                            {newExerciseData!.alternativeNamesEn.join(", ")}
+                        </li>
+                        <li>Description EN: {newExerciseData!.descriptionEn}</li>
+                        <li>Category ID: {newExerciseData!.category}</li>
+                        <li>Muscles: {newExerciseData!.muscles}</li>
+                        <li>Muscles secondary: {newExerciseData!.musclesSecondary}</li>
+                        <li>Variation ID: {newExerciseData!.variationId}</li>
+                        <li>Language ID: {newExerciseData!.languageId}</li>
+                        <li>Name translation: {newExerciseData!.nameTranslation}</li>
+                        <li>
+                            Description translation: {newExerciseData!.descriptionTranslation}
+                        </li>
+                        <li>
+                            Alternative names translation:{" "}
+                            {newExerciseData!.alternativeNamesTranslation}
+                        </li>
+                        <li>Images: {newExerciseData!.images}</li>
+                    </ul>
+                </Paper>
+            </Stack>
+            <Box>
+                <Typography gutterBottom variant="h5" component="div">
+                    {t("exercises.contribute-exercise-description")}
+                </Typography>
+                <Stepper activeStep={activeStep} orientation="vertical">
+                    <Step key={1}>
+                        <StepLabel>Basics in English</StepLabel>
+                        <StepContent>
+                            <Step1Basics
+                                onContinue={handleNext}
+                                setNewExerciseData={setNewExerciseData}
+                                newExerciseData={newExerciseData}
+                            />
+                        </StepContent>
+                    </Step>
+                    <Step key={2}>
+                        <StepLabel>Variations</StepLabel>
+                        <StepContent>
+                            <Step2Variations onContinue={handleNext} onBack={handleBack} />
+                        </StepContent>
+                    </Step>
+                    <Step key={3}>
+                        <StepLabel>Description</StepLabel>
+                        <StepContent>
+                            <Step3Description
+                                onContinue={handleNext}
+                                onBack={handleBack}
+                                setNewExerciseData={setNewExerciseData}
+                                newExerciseData={newExerciseData}
+                            />
+                        </StepContent>
+                    </Step>
+                    <Step key={4}>
+                        <StepLabel>Translation</StepLabel>
+                        <StepContent>
+                            <Step4Translations onContinue={handleNext}
+                                               onBack={handleBack}
+                                               setNewExerciseData={setNewExerciseData}
+                                               newExerciseData={newExerciseData} />
+                        </StepContent>
+                    </Step>
+                    <Step key={5}>
+                        <StepLabel>Images</StepLabel>
+                        <StepContent>
+                            <Step5Images
+                                onContinue={handleNext}
+                                onBack={handleBack}
+                                setNewExerciseData={setNewExerciseData}
+                                newExerciseData={newExerciseData}
+                            />
+                        </StepContent>
+                    </Step>
+                </Stepper>
+                {activeStep === 5 && (
+                    <Paper square sx={{ p: 3 }}>
+                        <Typography>Exercise was successfully submitted!</Typography>
+                        <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+                            Reset
+                        </Button>
+                    </Paper>
+                )}
+            </Box>
+        </Container>
+    );
 };
