cd6728984e5106189e3ee00fa5601a7a9f29c67a
Roland Geider <roland@geider.net>
2022-07-19 00:03:12 +0200

    Add simple tests for string helpers

---
+++ src/utils/strings.test.ts
@@ -0,0 +1,54 @@
+import { getTranslationKey, slugify, truncateLongNames } from "utils/strings";
+
+describe("slugify utilities", () => {
+
+    test('whitespace', () => {
+        expect(slugify('test 123')).toEqual('test_123');
+    });
+
+    test('several whitespaces', () => {
+        expect(slugify('test   123')).toEqual('test___123');
+    });
+
+    test('capital letters', () => {
+        expect(slugify('TeST123')).toEqual('test123');
+    });
+
+    test('special chars are kept', () => {
+        expect(slugify('abc*/&%$§')).toEqual('abc*/&%$§');
+    });
+});
+
+describe("translation key utilities", () => {
+
+    test('key', () => {
+        expect(getTranslationKey('test 123')).toEqual('exercises.test_123');
+    });
+
+    test('several whitespaces', () => {
+        expect(getTranslationKey('test   123')).toEqual('exercises.test___123');
+    });
+
+    test('capital letters', () => {
+        expect(getTranslationKey('TeST123')).toEqual('exercises.test123');
+    });
+
+    test('special chars are kept', () => {
+        expect(getTranslationKey('abc*/&%$§')).toEqual('exercises.abc*/&%$§');
+    });
+});
+
+describe("truncate name utilities", () => {
+
+    test('long name, default max length', () => {
+        expect(truncateLongNames('Pizza is a dish of Italian origin consisting of...')).toEqual('Pizza is a dish of Ita…');
+    });
+
+    test('long name, custom max length', () => {
+        expect(truncateLongNames('Pizza is a dish of Italian origin consisting of a usually round...', 10)).toEqual('Pizza is a…');
+    });
+
+    test('short name, default max length', () => {
+        expect(truncateLongNames('Pizza is a dish')).toEqual('Pizza is a dish');
+    });
+});
