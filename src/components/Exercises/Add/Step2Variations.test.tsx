454fa892678b54841d5659c5be812e8879496915
Roland Geider <roland@geider.net>
2022-07-22 09:25:44 +0200

    Add render test for Step2Variations.tsx

---
+++ src/components/Exercises/Add/Step2Variations.test.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { testExerciseBenchPress, testExerciseCurls } from "tests/exerciseTestdata";
+import { useBasesQuery } from "components/Exercises/queries";
+import { Step2Variations } from "components/Exercises/Add/Step2Variations";
+
+jest.mock("components/Exercises/queries");
+const mockedUseBasesQuery = useBasesQuery as jest.Mock;
+let emptyExerciseData = {
+    category: "",
+    muscles: [],
+    musclesSecondary: [],
+    variationId: null,
+    languageId: null,
+    equipment: [],
+
+    nameEn: "",
+    descriptionEn: "",
+    alternativeNamesEn: [],
+
+    nameTranslation: "",
+    alternativeNamesTranslation: [],
+    descriptionTranslation: "",
+    images: [],
+};
+
+
+describe("Test the add exercise step 2 component", () => {
+
+    beforeEach(() => {
+        mockedUseBasesQuery.mockImplementation(() => ({
+            isLoading: false,
+            data: [testExerciseBenchPress, testExerciseCurls]
+        }));
+        emptyExerciseData = {
+            category: "",
+            muscles: [],
+            musclesSecondary: [],
+            variationId: null,
+            languageId: null,
+            equipment: [],
+
+            nameEn: "",
+            descriptionEn: "",
+            alternativeNamesEn: [],
+
+            nameTranslation: "",
+            alternativeNamesTranslation: [],
+            descriptionTranslation: "",
+            images: [],
+        };
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test("Renders without crashing", () => {
+        // Arrange
+        const mockOnContinue = jest.fn();
+        const mockSetExerciseData = jest.fn();
+
+        // Act
+        const queryClient = new QueryClient();
+        render(
+            <QueryClientProvider client={queryClient}>
+                <Step2Variations
+                    onContinue={mockOnContinue}
+                    newExerciseData={emptyExerciseData}
+                    setNewExerciseData={mockSetExerciseData} />
+            </QueryClientProvider>
+        );
+
+        // Assert
+        expect(screen.getByText("forms.whatVariationsExist")).toBeInTheDocument();
+        expect(screen.getByText("Benchpress")).toBeInTheDocument();
+        expect(screen.getByText("Curls")).toBeInTheDocument();
+    });
+});
