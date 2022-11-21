7ee8d7a18cd93fb91ec8f77aaf840525ac4dea4a
Mohammad Rafigh <mohammad.rafigh@gmail.com>
2022-11-21 15:52:56 +0330

    Rename variable to MIN_ACCOUNT_AGE_TO_TRUST

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,6 +1,6 @@
 export const ENGLISH_LANGUAGE_ID = 2;
 
-export const MIN_ACCOUNT_AGE = process.env.MINIMUM_AGE_TO_TRUST || 21;
+export const MIN_ACCOUNT_AGE = process.env.MIN_ACCOUNT_AGE_TO_TRUST || 21;
 
 export const QUERY_EXERCISE_BASES = 'bases';
 export const QUERY_EXERCISE_BASES_VARIATIONS = 'bases-variations';
