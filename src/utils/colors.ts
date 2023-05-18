7ec60e5fac2faaa03df3917005541a96d525c5d6
Roland Geider <roland@geider.net>
2023-05-18 16:21:36 +0200

    Add test for the chart color utility

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -5,7 +5,7 @@ export function* generateChartColors(nrOfItems: number) {
     let colors;
     if (nrOfItems <= 3) {
         colors = LIST_OF_COLORS3;
-    } else if (nrOfItems <= 8) {
+    } else if (nrOfItems <= 5) {
         colors = LIST_OF_COLORS5;
     } else {
         colors = LIST_OF_COLORS8;
