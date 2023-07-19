2c0bf541d78c6324944962c7baf84a62c2fecdc7
Roland Geider <roland@geider.net>
2023-07-19 20:21:01 +0200

    Fix test

--- src/components/Nutrition/components/PlanDetail.test.tsx
+++ src/components/Nutrition/components/PlanDetail.test.tsx
@@ -1,21 +1,36 @@
-import { render, screen } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { MemoryRouter, Route, Routes } from "react-router";
-import { TEST_NUTRITIONAL_PLAN_2 } from "tests/nutritionTestdata";
+import { render, screen } from '@testing-library/react';
 import { PlanDetail } from "components/Nutrition/components/PlanDetail";
 import { useFetchNutritionalPlanQuery } from "components/Nutrition/queries";
+import { MemoryRouter, Route, Routes } from "react-router";
+import { TEST_NUTRITIONAL_PLAN_2 } from "tests/nutritionTestdata";
 
 jest.mock("components/Nutrition/queries");
 
+const { ResizeObserver } = window;
 const queryClient = new QueryClient();
 
 describe("Test the PlanDetail component", () => {
 
     beforeEach(() => {
+        // @ts-ignore
         useFetchNutritionalPlanQuery.mockImplementation(() => ({
             isSuccess: true,
             data: TEST_NUTRITIONAL_PLAN_2
         }));
+
+        // @ts-ignore
+        delete window.ResizeObserver;
+        window.ResizeObserver = jest.fn().mockImplementation(() => ({
+            observe: jest.fn(),
+            unobserve: jest.fn(),
+            disconnect: jest.fn()
+        }));
+    });
+
+    afterEach(() => {
+        window.ResizeObserver = ResizeObserver;
+        jest.restoreAllMocks();
     });
 
     test('renders the current nutritional plan correctly', async () => {
