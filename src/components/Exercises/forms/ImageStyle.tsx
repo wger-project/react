33fb2d626e907d6e724966d5b7e1d1784f77dc2e
Roland Geider <roland@geider.net>
2024-01-12 21:02:09 +0100

    Rename variables and cleanup

--- src/components/Exercises/forms/ImageStyle.tsx
+++ src/components/Exercises/forms/ImageStyle.tsx
@@ -12,55 +12,52 @@ import { useTranslation } from "react-i18next";
 export function ImageStyleToggle(props: { fieldName: string }) {
     const [t] = useTranslation();
 
-    const [alignment, setAlignment] = React.useState<string | null>('left');
+    const [style, setStyle] = React.useState<string | null>('photo');
 
     const handleAlignment = (
         event: React.MouseEvent<HTMLElement>,
         newAlignment: string | null,
     ) => {
-        setAlignment(newAlignment);
+        setStyle(newAlignment);
     };
 
     return (
         <ToggleButtonGroup
-            value={alignment}
+            value={style}
             exclusive
             fullWidth
             onChange={handleAlignment}
             aria-label="text alignment"
         >
-            <ToggleButton value="photo" aria-label="left aligned">
+            <ToggleButton value="photo">
                 <Stack justifyContent="center" alignItems="center">
                     <PhotoCameraIcon />
                     <Typography variant="caption">{t('exercises.imageStylePhoto')}</Typography>
                 </Stack>
             </ToggleButton>
 
-            <ToggleButton value="3d" aria-label="centered">
+            <ToggleButton value="3d">
                 <Stack justifyContent="center" alignItems="center">
                     <LandscapeIcon />
                     <Typography variant="caption">{t('exercises.imageStyle3D')}</Typography>
                 </Stack>
-
             </ToggleButton>
 
-            <ToggleButton value="line" aria-label="right aligned">
+            <ToggleButton value="line">
                 <Stack justifyContent="center" alignItems="center">
                     <LandscapeOutlinedIcon />
                     <Typography variant="caption">{t('exercises.imageStyleLine')}</Typography>
                 </Stack>
-
             </ToggleButton>
 
-            <ToggleButton value="low-poly" aria-label="justified">
+            <ToggleButton value="low-poly">
                 <Stack justifyContent="center" alignItems="center">
                     <LandscapeTwoToneIcon />
                     <Typography variant="caption">{t('exercises.imageStyleLowPoly')}</Typography>
                 </Stack>
-
             </ToggleButton>
 
-            <ToggleButton value="other" aria-label="justified">
+            <ToggleButton value="other">
                 <Stack justifyContent="center" alignItems="center">
                     <MoreHorizIcon />
                     <Typography variant="caption">{t('exercises.imageStyleOther')}</Typography>
