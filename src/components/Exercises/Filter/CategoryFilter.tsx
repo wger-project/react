0591708b768702246130e2272ec57c8104a802d1
StijnKing <s.j.m.nooijen@gmail.com>
2024-04-01 21:06:37 +0200

    Add a drawer with filters for mobile use
    On the exercise overview page, the filters are now placed
    in a drawer.

--- src/components/Exercises/Filter/CategoryFilter.tsx
+++ src/components/Exercises/Filter/CategoryFilter.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
-import { List, ListItem, ListItemButton, ListItemIcon, ListItemText, Paper, Switch, Typography } from "@mui/material";
+import React, { useContext } from 'react';
+import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Paper,
+    Switch,
+    Typography
+} from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { Category } from "components/Exercises/models/category";
 import { getTranslationKey } from "utils/strings";
+import { useCategoriesQuery } from '../queries';
+import { ExerciseFiltersContext } from './ExerciseFiltersContext';
+import { LoadingPlaceholder } from '../../Core/LoadingWidget/LoadingWidget';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
+const CategoryFilterList = () => {
 
-type CategoryFilterProps = {
-    categories: Category[];
-    selectedCategories: Category[];
-    setSelectedCategories: (categories: Category[]) => void;
-}
-
-export const CategoryFilter = ({ categories, selectedCategories, setSelectedCategories }: CategoryFilterProps) => {
-
+    const { data: categories, isLoading } = useCategoriesQuery();
+    const { selectedCategories, setSelectedCategories } = useContext(ExerciseFiltersContext);
     const [t] = useTranslation();
 
     const handleToggle = (value: Category) => () => {
@@ -28,39 +39,65 @@ export const CategoryFilter = ({ categories, selectedCategories, setSelectedCate
         setSelectedCategories(newChecked);
     };
 
+    if (isLoading) {
+        return <LoadingPlaceholder />;
+    }
+
+    return (
+        <List>
+            {categories!.map((category) => {
+                const labelId = `checkbox-list-label-${category.id}`;
+
+                return (
+                    <ListItem
+                        key={category.id}
+                        disablePadding
+                    >
+                        <ListItemButton role={undefined} onClick={handleToggle(category)} dense>
+                            <ListItemIcon>
+                                <Switch
+                                    key={`category-${category.id}`}
+                                    edge="start"
+                                    checked={selectedCategories.indexOf(category) !== -1}
+                                    tabIndex={-1}
+                                    disableRipple
+                                    inputProps={{ 'aria-labelledby': labelId }}
+                                />
+                            </ListItemIcon>
+                            <ListItemText id={labelId} primary={t(getTranslationKey(category.name))} />
+                        </ListItemButton>
+                    </ListItem>
+                );
+            })}
+        </List>
+    );
+};
+
+export const CategoryFilterDropdown = () => {
+    const [t] = useTranslation();
+
+    return (
+        <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                {t('category')}
+            </AccordionSummary>
+            <AccordionDetails>
+                <CategoryFilterList />
+            </AccordionDetails>
+        </Accordion>
+    );
+};
+
+export const CategoryFilter = () => {
+    const [t] = useTranslation();
+
     return (
         <div data-testid={"categories"}>
             <Paper>
                 <Typography gutterBottom variant="h6" m={2}>
                     {t('category')}
                 </Typography>
-
-                <List>
-                    {categories.map((category) => {
-                        const labelId = `checkbox-list-label-${category.id}`;
-
-                        return (
-                            <ListItem
-                                key={category.id}
-                                disablePadding
-                            >
-                                <ListItemButton role={undefined} onClick={handleToggle(category)} dense>
-                                    <ListItemIcon>
-                                        <Switch
-                                            key={`category-${category.id}`}
-                                            edge="start"
-                                            checked={selectedCategories.indexOf(category) !== -1}
-                                            tabIndex={-1}
-                                            disableRipple
-                                            inputProps={{ 'aria-labelledby': labelId }}
-                                        />
-                                    </ListItemIcon>
-                                    <ListItemText id={labelId} primary={t(getTranslationKey(category.name))} />
-                                </ListItemButton>
-                            </ListItem>
-                        );
-                    })}
-                </List>
+                <CategoryFilterList />
             </Paper>
         </div>
     );
--- src/components/Exercises/Filter/MuscleFilter.tsx
+++ src/components/Exercises/Filter/MuscleFilter.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
     IconButton,
     List,
     ListItem,
@@ -16,15 +19,15 @@ import { Muscle } from "components/Exercises/models/muscle";
 import { getTranslationKey } from "utils/strings";
 import { MuscleOverview } from "components/Muscles/MuscleOverview";
 import { LightTooltip } from "components/Core/Tooltips/LightToolTip";
+import { useMusclesQuery } from '../queries';
+import { ExerciseFiltersContext } from './ExerciseFiltersContext';
+import { LoadingPlaceholder } from '../../Core/LoadingWidget/LoadingWidget';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-type MuscleFilterProps = {
-    muscles: Muscle[];
-    selectedMuscles: Muscle[];
-    setSelectedMuscles: (muscles: Muscle[]) => void;
-}
+const MuscleFilterList = () => {
 
-export const MuscleFilter = ({ muscles, selectedMuscles, setSelectedMuscles }: MuscleFilterProps) => {
-
+    const { data: muscles, isLoading } = useMusclesQuery();
+    const { selectedMuscles, setSelectedMuscles } = useContext(ExerciseFiltersContext);
     const [t] = useTranslation();
 
     const handleToggle = (value: Muscle) => () => {
@@ -40,59 +43,88 @@ export const MuscleFilter = ({ muscles, selectedMuscles, setSelectedMuscles }: M
         setSelectedMuscles(newChecked);
     };
 
+    if (isLoading) {
+        return <LoadingPlaceholder />;
+    }
+
+    return (
+        <List sx={{ maxHeight: "500px", overflowY: "auto" }}>
+            {muscles!.map((m) => {
+                const labelId = `checkbox-list-label-${m.id}`;
+
+                return (
+                    <ListItem
+                        key={m.id}
+                        disablePadding
+                        secondaryAction={
+                            <LightTooltip
+                                title={
+                                    <MuscleOverview
+                                        primaryMuscles={[m]}
+                                        secondaryMuscles={[]}
+                                        isFront={m.isFront}
+                                    />
+                                }
+                                placement="right"
+                                arrow>
+                                <IconButton edge="end" aria-label="comments">
+                                    <InfoOutlinedIcon />
+                                </IconButton>
+                            </LightTooltip>
+                        }
+                    >
+                        <ListItemButton role={undefined} onClick={handleToggle(m)} dense>
+                            <ListItemIcon>
+                                <Switch
+                                    key={`muscle-${m.id}`}
+                                    edge="start"
+                                    checked={selectedMuscles.indexOf(m) !== -1}
+                                    tabIndex={-1}
+                                    disableRipple
+                                    inputProps={{ 'aria-labelledby': labelId }}
+                                />
+                            </ListItemIcon>
+
+                            <ListItemText
+                                id={labelId}
+                                primary={m.name}
+                                secondary={m.nameEn !== '' ? t(getTranslationKey(m.nameEn)) : ''} />
+
+                        </ListItemButton>
+                    </ListItem>
+                );
+            })}
+        </List>
+    );
+};
+
+export const MuscleFilterDropdown = () => {
+
+    const [t] = useTranslation();
+
+    return (
+        <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                {t('exercises.muscles')}
+            </AccordionSummary>
+            <AccordionDetails>
+                <MuscleFilterList />
+            </AccordionDetails>
+        </Accordion>
+    );
+};
+
+export const MuscleFilter = () => {
+
+    const [t] = useTranslation();
+
     return (
         <div data-testid={"muscles"}>
             <Paper sx={{ mt: 2 }}>
                 <Typography gutterBottom variant="h6" m={2}>
                     {t('exercises.muscles')}
                 </Typography>
-                <List sx={{ maxHeight: "500px", overflowY: "auto" }}>
-                    {muscles.map((m) => {
-                        const labelId = `checkbox-list-label-${m.id}`;
-
-                        return (
-                            <ListItem
-                                key={m.id}
-                                disablePadding
-                                secondaryAction={
-                                    <LightTooltip
-                                        title={
-                                            <MuscleOverview
-                                                primaryMuscles={[m]}
-                                                secondaryMuscles={[]}
-                                                isFront={m.isFront}
-                                            />
-                                        }
-                                        placement="right"
-                                        arrow>
-                                        <IconButton edge="end" aria-label="comments">
-                                            <InfoOutlinedIcon />
-                                        </IconButton>
-                                    </LightTooltip>
-                                }
-                            >
-                                <ListItemButton role={undefined} onClick={handleToggle(m)} dense>
-                                    <ListItemIcon>
-                                        <Switch
-                                            key={`muscle-${m.id}`}
-                                            edge="start"
-                                            checked={selectedMuscles.indexOf(m) !== -1}
-                                            tabIndex={-1}
-                                            disableRipple
-                                            inputProps={{ 'aria-labelledby': labelId }}
-                                        />
-                                    </ListItemIcon>
-
-                                    <ListItemText
-                                        id={labelId}
-                                        primary={m.name}
-                                        secondary={m.nameEn !== '' ? t(getTranslationKey(m.nameEn)) : ''} />
-
-                                </ListItemButton>
-                            </ListItem>
-                        );
-                    })}
-                </List>
+                <MuscleFilterList />
             </Paper>
         </div>
     );
