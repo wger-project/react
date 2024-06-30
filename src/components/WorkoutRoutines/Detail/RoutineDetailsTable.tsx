e78228a547b8c8e75024f169ba7885f31061d42c
Roland Geider <roland@geider.net>
2024-06-30 15:02:45 +0200

    Some polishing

--- src/components/WorkoutRoutines/Detail/RoutineDetailsTable.tsx
+++ src/components/WorkoutRoutines/Detail/RoutineDetailsTable.tsx
@@ -8,7 +8,8 @@ import {
     TableContainer,
     TableHead,
     TableRow,
-    Typography
+    Typography,
+    useTheme
 } from "@mui/material";
 import { LoadingPlaceholder } from "components/Core/LoadingWidget/LoadingWidget";
 import { RoutineDayData } from "components/WorkoutRoutines/models/RoutineDayData";
@@ -24,7 +25,9 @@ export const RoutineDetailsTable = () => {
     const routineId = params.routineId ? parseInt(params.routineId) : 0;
     const routineQuery = useRoutineDetailQuery(routineId);
 
-    return <Container maxWidth={false}>
+
+    //maxWidth={false}
+    return <Container maxWidth={false} sx={{ overflowX: 'scroll' }}>
         {routineQuery.isLoading
             ? <LoadingPlaceholder />
             : <>
@@ -49,27 +52,25 @@ export const RoutineDetailsTable = () => {
 
 const DayTable = (props: { dayData: RoutineDayData[], iteration: number }) => {
     const [t] = useTranslation();
+    const theme = useTheme();
 
     return <>
-        <TableContainer component={Paper} sx={{ maxWidth: 500 }}>
-            <Table size="small" aria-label="simple table">
+        <TableContainer component={Paper} sx={{ minWidth: 510 }}>
+            <Table size="small">
                 <TableHead>
                     <TableRow>
-                        <TableCell>
+                        <TableCell colSpan={6}>
                             <Typography variant={'h5'}>
                                 Week {props.iteration}
                             </Typography>
                         </TableCell>
-                        <TableCell></TableCell>
-                        <TableCell></TableCell>
-                        <TableCell></TableCell>
-                        <TableCell></TableCell>
                     </TableRow>
                     <TableRow>
                         <TableCell></TableCell>
                         <TableCell align="right">Sets</TableCell>
                         <TableCell align="right">Reps</TableCell>
                         <TableCell align="right">Weight</TableCell>
+                        <TableCell align="right">Rest</TableCell>
                         <TableCell align="right">RiR</TableCell>
                     </TableRow>
                 </TableHead>
@@ -77,31 +78,52 @@ const DayTable = (props: { dayData: RoutineDayData[], iteration: number }) => {
                     {props.dayData.map((dayData, index) =>
                         <>
                             <TableRow>
-                                <TableCell><b>{dayData.day.isRest ? t('routines.restDay') : dayData.day.name}</b></TableCell>
-                                <TableCell></TableCell>
-                                <TableCell></TableCell>
-                                <TableCell></TableCell>
-                                <TableCell></TableCell>
+                                <TableCell
+                                    sx={{ backgroundColor: dayData.day.isRest ? theme.palette.action.hover : null }}
+                                    colSpan={6}
+                                >
+                                    <b>{dayData.day.isRest ? t('routines.restDay') : dayData.day.name}</b>
+                                </TableCell>
                             </TableRow>
                             {dayData.slots.map((slotData) =>
                                 <>
                                     {slotData.setConfigs.map((setConfig) =>
                                         <TableRow>
-                                            <TableCell>{setConfig.exercise?.getTranslation().name}</TableCell>
-                                            <TableCell align="right">{setConfig.nrOfSets}</TableCell>
-                                            <TableCell align="right">{setConfig.reps}</TableCell>
-                                            <TableCell align="right">{setConfig.weight}</TableCell>
-                                            <TableCell align="right">{setConfig.rir}</TableCell>
+                                            <TableCell padding={'normal'}>
+                                                {setConfig.exercise?.getTranslation().name}
+                                                {/*ID: {setConfig.slotConfigId}*/}
+                                            </TableCell>
+                                            <TableCell padding={'none'} align={'center'}>
+                                                {setConfig.nrOfSets}
+                                            </TableCell>
+                                            <TableCell padding={'none'} align={'center'}>
+                                                {setConfig.reps}
+                                                {setConfig.maxReps !== null &&
+                                                    <> - {setConfig.maxReps}</>
+                                                }
+                                            </TableCell>
+                                            <TableCell padding={'none'} align={'center'}>
+                                                {setConfig.weight}
+                                                {setConfig.maxWeight !== null &&
+                                                    <> - {setConfig.maxWeight}</>
+                                                }
+                                            </TableCell>
+                                            <TableCell padding={'none'} align={'center'}>
+                                                {setConfig.restTime}
+                                                {setConfig.maxRestTime !== null &&
+                                                    <> - {setConfig.maxRestTime}</>
+                                                }
+                                            </TableCell>
+
+                                            <TableCell padding={'normal'} align={'center'}>
+                                                {setConfig.rir}
+                                            </TableCell>
                                         </TableRow>
                                     )}
                                 </>
                             )}
                             <TableRow>
-                                <TableCell></TableCell>
-                                <TableCell></TableCell>
-                                <TableCell></TableCell>
-                                <TableCell></TableCell>
-                                <TableCell></TableCell>
+                                <TableCell colSpan={6}></TableCell>
                             </TableRow>
                         </>
                     )}
