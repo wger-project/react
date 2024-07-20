d26728605f83ef2e12d73de682dc3858ec6f327d
Roland Geider <roland@geider.net>
2024-07-20 13:17:03 +0200

    Fix paths and variable names

--- postbuild.sh
+++ postbuild.sh
@@ -8,8 +8,8 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 if [ -d "$STATIC_FOLDER" ]; then
-    cp build/static/js/main.*.js $STATIC_FOLDER/main.js
-    cp build/static/css/main.*.css $STATIC_FOLDER/main.css
+    cp build/assets/index-*.js $STATIC_FOLDER/main.js
+    cp build/assets/index-*.css $STATIC_FOLDER/main.css
     cp -r build/locales $STATIC_FOLDER/
     cp -r build/muscles $STATIC_FOLDER/
     echo -e "${GREEN}*** SUCCESS ***: Build files copied to django static folder: ${STATIC_FOLDER} ${NC}"
--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
  *  Collect all constants that access import.meta.env so it's easier to mock them
  */
 
-export const IS_PROD = import.meta.env.NODE_ENV === "production";
+export const IS_PROD = import.meta.env.PROD;
 export const PUBLIC_URL = IS_PROD ? "/static/react" : import.meta.env.PUBLIC_URL;
 export const SERVER_URL = IS_PROD ? "" : import.meta.env.VITE_API_SERVER;
 export const TIME_ZONE = import.meta.env.TIME_ZONE;
