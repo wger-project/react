5df9d7133dcf27df91c1a985e5aae7df09263c08
Roland Geider <roland@geider.net>
2025-01-07 20:34:33 +0100

    Remove the fab on the template overviews

--- src/components/WorkoutRoutines/Overview/PrivateTemplateOverview.tsx
+++ src/components/WorkoutRoutines/Overview/PrivateTemplateOverview.tsx
@@ -2,7 +2,6 @@ import { List, Paper, } from "@mui/material";
 import { LoadingPlaceholder } from "components/Core/LoadingWidget/LoadingWidget";
 import { WgerContainerRightSidebar } from "components/Core/Widgets/Container";
 import { OverviewEmpty } from "components/Core/Widgets/OverviewEmpty";
-import { AddRoutineFab } from "components/WorkoutRoutines/Overview/Fab";
 import { RoutineList } from "components/WorkoutRoutines/Overview/RoutineOverview";
 import { usePrivateRoutinesShallowQuery } from "components/WorkoutRoutines/queries/routines";
 import React from "react";
@@ -34,6 +33,5 @@ export const PrivateTemplateOverview = () => {
                     </List>
                 </Paper>}
         </>}
-        fab={<AddRoutineFab />}
     />;
 };
--- src/components/WorkoutRoutines/Overview/PublicTemplateOverview.tsx
+++ src/components/WorkoutRoutines/Overview/PublicTemplateOverview.tsx
@@ -2,7 +2,6 @@ import { List, Paper, } from "@mui/material";
 import { LoadingPlaceholder } from "components/Core/LoadingWidget/LoadingWidget";
 import { WgerContainerRightSidebar } from "components/Core/Widgets/Container";
 import { OverviewEmpty } from "components/Core/Widgets/OverviewEmpty";
-import { AddRoutineFab } from "components/WorkoutRoutines/Overview/Fab";
 import { RoutineList } from "components/WorkoutRoutines/Overview/RoutineOverview";
 import { usePublicRoutinesShallowQuery } from "components/WorkoutRoutines/queries";
 import React from "react";
@@ -34,6 +33,5 @@ export const PublicTemplateOverview = () => {
                     </List>
                 </Paper>}
         </>}
-        fab={<AddRoutineFab />}
     />;
 };
