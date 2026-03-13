1632cb2aed1680f6a3d12df123883393bfea901a
Roland Geider <roland@geider.net>
2026-03-13 12:36:34 +0100

    Update vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import eslintPlugin from "vite-plugin-eslint";
-import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(() => {
     return {
+        resolve: {
+            tsconfigPaths: true
+        },
         build: {
             outDir: 'build',
             sourcemap: true,
@@ -27,7 +29,6 @@ export default defineConfig(() => {
                     plugins: ['@emotion/babel-plugin'],
                 },
             }),
-            viteTsconfigPaths(),
             eslintPlugin({
                 cache: false,
                 include: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
