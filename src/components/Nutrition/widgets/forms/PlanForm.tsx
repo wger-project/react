ba7c734213d17b8a012f38f5fbcc1ab2a687c9f8
Roland Geider <roland@geider.net>
2026-09-07 18:19:03 +0200

    Start the goals switch of the plan form controlled

--- src/components/Nutrition/widgets/forms/PlanForm.tsx
+++ src/components/Nutrition/widgets/forms/PlanForm.tsx
@@ -57,7 +57,7 @@ export const PlanForm = ({ plan, closeFn }: PlanFormProps) => {
     const [t] = useTranslation();
     const addPlanQuery = useAddNutritionalPlanQuery();
     const editPlanQuery = useEditNutritionalPlanQuery(plan?.id ?? '');
-    const [useGoals, setUseGoals] = useState(plan?.hasAnyGoals);
+    const [useGoals, setUseGoals] = useState(plan?.hasAnyGoals ?? false);
     const [startDateValue, setStartDateValue] = useState<DateTime | null>(plan ? DateTime.fromJSDate(plan.start) : DateTime.now);
     const [endDateValue, setEndDateValue] = useState<DateTime | null>(plan && plan?.end !== null ? DateTime.fromJSDate(plan!.end) : null);
 
